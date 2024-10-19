c1d662989c5d46cc0f8f19d2e338836cefa5da07
martinrumisek <mrumisek@seznam.cz>
2024-10-19 16:07:20 +0200

    Využití knihovny datatables.

--- app/Views/race_page.php
+++ app/Views/race_page.php
@@ -8,6 +8,7 @@
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
     <!-- Flag Icons CSS from https://flagicons.lipis.dev/ -->
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/gh/lipis/flag-icons@6.7.0/css/flag-icons.min.css"/>
+    <link rel="stylesheet" href="https://cdn.datatables.net/1.10.21/css/jquery.dataTables.min.css">
     <title>Race (závod) - <?php echo $race->default_name;?></title>
     <style>
         .race-info {
@@ -28,21 +29,33 @@
         <!-- Název závodu -->
         <h1><?php echo $race->default_name; ?></h1>
 
-        <table class="table">
+        <table class="display" id="raceTable">
+            <thead>
+                <tr>
+                    <th>Vlajka</th>
+                    <th>Zkratka země</th>
+                    <th>Rok</th>
+                </tr>
+            </thead>
             <tbody>
                 <!-- Loop přes závody -->
                 <?php foreach($race_years as $race_year) { ?>
                     <tr>
-                        <td>
-                            <div class="race-country">
-                                <span class="fi fi-<?php echo strtolower($race_year->country); ?>"></span> <!-- Vlajka podle zkratky země -->
-                                <span><?php echo $race_year->country; ?> - Rok: <?php echo $race_year->year; ?></span>
-                            </div>
-                        </td>
+                        <td><span class="fi fi-<?php echo strtolower($race_year->country); ?>"></span></td>
+                        <td><?php echo $race_year->country; ?></td>
+                        <td><?php echo $race_year->year; ?></td>
                     </tr>
                 <?php } ?>
             </tbody>
         </table>
     </div>
+    <script src="https://code.jquery.com/jquery-3.5.1.min.js"></script>
+    <script src="https://cdn.datatables.net/1.10.21/js/jquery.dataTables.min.js"></script>
+
+    <script>
+        $(document).ready(function() {
+            $('#raceTable').DataTable(); // Inicializace DataTables
+        });
+    </script>
 </body>
 </html>
