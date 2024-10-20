85e873051b30b9d0014d3b12959ea69da1a8fff6
David Žižlavský <davidcitron2@seznam.cz>
2024-10-20 23:34:22 +0200

    Úprava titulku stránky login

--- app/Views/login.php
+++ app/Views/login.php
@@ -8,7 +8,7 @@
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
     <!-- Flag Icons CSS from https://flagicons.lipis.dev/ -->
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/gh/lipis/flag-icons@6.7.0/css/flag-icons.min.css"/>
-    <title>Register</title>
+    <title>Login</title>
     <style>
         .ftco-section {
             padding: 7em 0; }
