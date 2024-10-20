c437695842c345da64b0eecc3dd3ce81627adee9
David Žižlavský <davidcitron2@seznam.cz>
2024-10-20 22:40:02 +0200

    Aktualizace templatu karet

--- app/Views/race_cards.php
+++ app/Views/race_cards.php
@@ -1,18 +1,22 @@
 <?php foreach($races as $race) { ?>
-    <div class="card m-1 race-card" style="height: 130px">
-        <div class="card-body">
-            <h5 class="card-title">
-                <?php echo $race->default_name; ?>
-            </h5>
-            <p class="card-text">
-                <a href="<?php echo base_url('race/'.$race->id)?>" class="text-decoration-none">
-                    Více informací o závodu
+    <div class="card m-1 race-card" style="width: 18rem; min-height: 12rem">
+        <div class="card-body d-flex flex-column justify-content-between">
+            <div class="d-flex justify-content-center align-items-center h-100">
+                <span class="card-title text-center fs-5 fw-normal m-0" style="display: inline-block; vertical-align: middle;">
+                    <?php echo $race->default_name; ?>
+                </span>
+            </div>
+            
+            <div class="mt-auto w-100 d-flex flex-column justify-content-center">
+                <!--
+                    <a href="<?php echo base_url('generate-pdf/'.$race->id)?>" class="d-flex justify-content-center text-decoration-none w-100 p-1 button-right" target="_blank">
+                        Show PDF <i class="fa fa-external-link fa-xs px-1 py-2"></i>
+                    </a>
+                -->
+                <a href="<?php echo base_url('race/'.$race->id)?>" class="d-flex justify-content-center text-decoration-none w-100 p-1 button-left">
+                    <button class="btn w-75 btn-primary btn-details">Details</button>
                 </a>
-                <br>
-                <a href="<?php echo base_url('generate-pdf/'.$race->id)?>" class="text-decoration-none" target="_blank">
-                    Zobrazit v PDF
-                </a>
-            </p>
+            </div>
         </div>
     </div>
 <?php } ?>
